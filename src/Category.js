13f33032bc9d354f5aa5fd18f14c4127a20e5847
soxterman <markoj2203@gmail.com>
2019-09-17 16:19:18 +0200

    New changes

--- src/Category.js
+++ src/Category.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Category() {
   return (
     <div className="columns">
-    	<div className="column categoryPos"><div className="circleBase type1"><img src="asociacije.png" alt="Asociacije" /></div></div>
-    	<div className="column categoryPos"><div className="circleBase type2"><img src="pantomima.png" alt="Pantomima" /></div></div>
-    	<div className="column categoryPos"><div className="circleBase type3"><img src="drawing.png" alt="Crtanje" /></div></div>
+    	<div className="column categoryPos"><div className="circleBase type1"><img className="imageStyle" src="asociacije.png" alt="Asociacije" /></div></div>
+    	<div className="column categoryPos"><div className="circleBase type1"><img className="imageStyle" src="pantomima.png" alt="Pantomima" /></div></div>
+    	<div className="column categoryPos"><div className="circleBase type1"><img className="imageStyle" src="drawing.png" alt="Crtanje" /></div></div>
     </div>
   );
 }
